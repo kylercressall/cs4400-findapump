97d444ac592ba98662ca2dd5f4f6ae7d51a94971
Nathan Carlson <10991314@uvu.edu>
2026-04-07 12:18:21 -0600

    Updated API variable name

--- find-a-pump-code/apps/backend/src/services/price.services.ts
+++ find-a-pump-code/apps/backend/src/services/price.services.ts
@@ -13,7 +13,7 @@ export const getAllPrices = async () => {
 };
 
 export const getFuelPricesByPlaceId = async (placeId: string) => {
-  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
   if (!apiKey) {
     throw new Error("Missing Google Maps API key");
